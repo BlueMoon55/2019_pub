3f08e8550a55ee3ca66f83cc7e753948de5d3499
BlueMoon55 <mskn55@gmail.com>
2020-09-24 21:53:17 +0900

    2nd Commit

---
+++ MindManager2/DAX46-01次世代情報システム構築フレームワーク.mmap4418080864734764778.mm
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="UTF-8"?><map xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:pri="http://schemas.mindjet.com/MindManager/Primitive/2003" xmlns:cor="http://schemas.mindjet.com/MindManager/Core/2003" xmlns:ap="http://schemas.mindjet.com/MindManager/Application/2003" version="0.9.0">
+<node TEXT="DAX46-01次世代情報システム構築フレームワーク" POSITION="left">
+<node TEXT="改版履歴" POSITION="right">
+<node TEXT="2020年3月27日 Xmindは使用せず、内容再構成" POSITION="right"/>
+<node TEXT="2020年3月18日 Xmindなし、リニューアル中" POSITION="right"/>
+<node TEXT="2019年3月12日　DAX43-01と併合及び分割" POSITION="right"/>
+<node TEXT="2019年3月6日　0.11版" POSITION="right"/>
+<node TEXT="2019年3月4日　0版" POSITION="right"/>
+</node>
+<node TEXT="用語" POSITION="right">
+<node TEXT="第4次産業革命, Society5.0, Connected Industriesの関係" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Society5.0" POSITION="right">
+<node TEXT="第４次産業革命技術がもたらす変化／新たな展開" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="目指すところの概念" POSITION="right">
+<node TEXT="新産業ビジョン" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Society5.0で実現する社会" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<node TEXT="Society5.0では、 サイバー空間とフィジカル空間を高度に融合させることにより、 多様なニーズにきめ細かく対応したモノやサービスを提供し、 経済的発展と社会的課題の解決を両立させる世界" POSITION="right"/>
+<node TEXT="Society5.0に向けて、 様々なつながりによる新たな価値社会を創出するConnected Industriesの実現に向けた当たらな産業構造の構築が必要" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="これまでの情報社会(4.0)からSociety5.0への技術の変化" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Society5.0で実現する社会" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Connected Industriesの考え方" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="社会の進展／国の施策" POSITION="right">
+<node TEXT="統合イノベーション戦略の推進体制" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+<node TEXT="データ連携基盤の整備" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+<icon BUILTIN="full-2"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DXとは" POSITION="right">
+<node TEXT="Wikipediaによると" POSITION="right">
+<node TEXT="「ITの浸透が、 人々の生活をあらゆる面でより良い方向に変化させる」という概念 【2004年にスウェーデンのウメオ大学のエリック・ストルターマン教授が提唱】" POSITION="right"/>
+<node TEXT="おおむね「企業がテクノロジーを利用して事業の業績や対象範囲を根底から変化させる」[2]という意味合い" POSITION="right"/>
+</node>
+<node TEXT="DX推進システムガイドラインによると" POSITION="right">
+<node TEXT=" 「企業がビジネス環境の激しい変化に対応し、 データとデジタル技術を活用して、 顧客や社会のニーズを基に、 製品やサービス、 ビジネスモデルを変革するとともに、 業務そのものや、 組織、 プロセス、 企業文化・風土を変革し、 競争上の優位性を確立すること。 」" POSITION="right"/>
+</node>
+<node TEXT="「日本の中小企業は、 とっくにDXを実践している」（2018年6月12日 Business Journal）" POSITION="right">
+<node TEXT="クラウドコンピューティングやIoT（モノのインターネット）、 AI（人工知能）、 ビッグデータ、 RPA（ロボットによる業務自動化）などの新しいICT（情報通信技術）を、 道具として活用して、 創出する変革という概念" POSITION="right"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DXのコンセプトと基本アクティビティ" POSITION="right">
+<node TEXT="DXに向けたスキル変革の方向性（概念図）" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXのアクティビティの戦略（屋根）" POSITION="right">
+<node TEXT="第4次産業革命" POSITION="right"/>
+<node TEXT="Society 5.0 / Connected Industriesの実現" POSITION="right">
+<node TEXT="Society 5.0の社会像" POSITION="right">
+<node TEXT="ＡＩ技術の発達" POSITION="right">
+<node TEXT="⇒定型的業務や数値的に表現可能な業務は、 ＡＩ技術により代替が可能に" POSITION="right"/>
+<node TEXT="⇒産業の変化、 働き方の変化" POSITION="right"/>
+</node>
+<node TEXT="日本の課題" POSITION="right">
+<node TEXT="ＡＩに関する研究開発に人材が不足、 少子高齢化、 つながりの希薄化、 自然体験の機会の減少" POSITION="right"/>
+</node>
+<node TEXT="人間の強み" POSITION="right">
+<node TEXT="現実世界を理解し意味づけできる感性、 倫理観、 板挟みや想定外と向き合い調整する力、 責任をもって遂行する力" POSITION="right"/>
+</node>
+</node>
+</node>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXのアクティビティの戦術（梁）" POSITION="right">
+<node TEXT="〔デジタルトランスフォーメーション（DX）〕を通した社会・企業・組織の自律的・継続的変革" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXによる価値創出のためのアクティビティ（家の中）" POSITION="right">
+<node TEXT="高速仮説検証サイクル（プログラミング思考）" POSITION="right"/>
+<node TEXT="イノベーション指向（デザイン思考・サービスサイエンス）" POSITION="right"/>
+<node TEXT="アジャイル・マインド＆プロセス" POSITION="right"/>
+<node TEXT="システムズエンジニアリング" POSITION="right"/>
+<node TEXT="API・産業/業務サービスインターフェース（IoTプラットフォーム）" POSITION="right"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXを支えるアクティビティ（柱）" POSITION="right">
+<node TEXT="創造性・人間性" POSITION="right">
+<node TEXT="理念" POSITION="right">
+<node TEXT="人間中心のカルチャーマインド" POSITION="right"/>
+</node>
+<node TEXT="活動の概念" POSITION="right">
+<node TEXT="・ 創造性・人間性とは、 個人個人の能力が社会の中で創造的かつ健全に開花すること" POSITION="right"/>
+<node TEXT="多様なチーム、 組織、 コミュニティに価値が提供できること" POSITION="right"/>
+<node TEXT="その中で生きがいを持って協働できる働きやすい社会を目指すこと。 " POSITION="right"/>
+<node TEXT="・人間中心は、 従来の顧客起点ではなく、 社会を構成する一人一人（顧客だけなく、 経営者も従業員も）の生きる意味を考えることが社会の価値につながるということ。 " POSITION="right"/>
+<node TEXT="サブトピック ITはそれをエンパワーするものであるべき。 " POSITION="right"/>
+</node>
+<node TEXT="具体的には" POSITION="right">
+<node TEXT="AIが人間をアシストする「インテリジェント・ワークプレイス」" POSITION="right">
+<node TEXT="AIという部下を使いこなす能力" POSITION="right"/>
+<node TEXT="AIではすぐに置換できない能力" POSITION="right"/>
+</node>
+<node TEXT="リベラルアーツ" POSITION="right"/>
+<node TEXT="ヒューマンスキル（iコンピテンシ・ディクショナリ）" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+</node>
+</node>
+<node TEXT="生産性・信頼性" POSITION="right">
+<node TEXT="理念" POSITION="right">
+<node TEXT="エンジニアリングの尊重と継承" POSITION="right"/>
+</node>
+<node TEXT="活動の概念" POSITION="right">
+<node TEXT="・ 生産性・信頼性とは、 技術力をもって生産性と品質・信頼性を担保するとともに、 " POSITION="right"/>
+<node TEXT="常に適切な技術とスキルを学習し、 それを社会に還元することを誇りとし、 " POSITION="right"/>
+<node TEXT="次世代に継承する努力を怠らない学習する組織・社会をめざすこと。 " POSITION="right"/>
+</node>
+<node TEXT="具体的には" POSITION="right">
+<node TEXT="従来型のシステム開発手法" POSITION="right">
+<node TEXT="共通フレーム" POSITION="right">
+<node TEXT="" POSITION="right"/>
+<node TEXT="目的は、 ソフトウェア開発に関係する人々（利害関係者）が、 「同じ言葉で話す」ことが出来るようにするため。 " POSITION="right"/>
+<node TEXT="ウォーターフォール、 スパイラル、 プロトタイプ、 アジャイル系すべての開発方法論に共通したもの。 " POSITION="right"/>
+</node>
+<node TEXT="政府情報システム構築ガイドライン、 実務者手引き" POSITION="right"/>
+<node TEXT="ウォータフォールモデル" POSITION="right"/>
+</node>
+<node TEXT="創造的なシステム開発手法" POSITION="right">
+<node TEXT="PoC（概念実証）" POSITION="right"/>
+<node TEXT="アジャイル" POSITION="right"/>
+<node TEXT="DevOpsによる迅速なソフト開発" POSITION="right"/>
+</node>
+<node TEXT="AIを活用したサービスの構築・運用の役割" POSITION="right">
+<node TEXT="IT人材" POSITION="right">
+<node TEXT="価値創造型： ユーザー企業の事業の価値創造を目的としたIT活用／要件が不確実、 スピード感を重視、 主にアジャイル型で開発" POSITION="right"/>
+<node TEXT="課題解決型： ユーザー企業の既存事業の効率化やコスト削減を目的としたIT活用／要件定義が明確、 確実性を重視、 主にウォーターフォール型で開発" POSITION="right"/>
+</node>
+<node TEXT="共通して求められる力：" POSITION="right">
+<node TEXT="文章や情報を正確に読み解き対話する力" POSITION="right"/>
+<node TEXT="科学的に思考・吟味し活用する力" POSITION="right"/>
+<node TEXT="価値を見つけ生み出す感性と力、 好奇心・探求力" POSITION="right"/>
+</node>
+<node TEXT="新たな社会を牽引する人材：" POSITION="right">
+<node TEXT="技術革新や価値創造の源となる飛躍知を発見・創造する人材" POSITION="right"/>
+<node TEXT="技術革新と社会課題をつなげ、 プラットフォームを創造する人材" POSITION="right"/>
+<node TEXT="様々な分野においてＡＩやデータの力を最大限活用し展開できる人材 等" POSITION="right"/>
+</node>
+<node TEXT="役割" POSITION="right">
+<node TEXT="マネージャ" POSITION="right"/>
+<node TEXT="データサイエンティスト" POSITION="right">
+<node TEXT="データアナリスト" POSITION="right"/>
+</node>
+<node TEXT="システムエンジニア" POSITION="right">
+<node TEXT="データエンジニア" POSITION="right"/>
+</node>
+</node>
+<node TEXT="データ分析の業務の流れ" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+</node>
+<node TEXT="システム構築の業務（タスク）と必要な技能・知識（スキル）" POSITION="right">
+<node TEXT="iコンピテンシ・ディクショナリ" POSITION="right"/>
+<node TEXT="ITSS+" POSITION="right">
+<node TEXT="IoTソリューション領域" POSITION="right"/>
+<node TEXT="データサイエンス領域" POSITION="right"/>
+<node TEXT="セキュリティ領域" POSITION="right"/>
+</node>
+</node>
+<node TEXT="Society5.0時代に適用可能と思われる技術、 普及が見込まれるサービス" POSITION="right">
+<node TEXT="サブトピック 1" POSITION="right"/>
+</node>
+</node>
+</node>
+<node TEXT="2つの柱で人間中心のイノベーションを" POSITION="right">
+<node TEXT="２本の柱でイノベーティブな社会変革を人間中心で仮説設定・検証を繰り返しながら進めていく。 特に技術中心で考えてきた日本の企業に対して人間中心イノベーションを個人・組織に植えつけることが必要。 本質を理解せずに形だけ真似しても成果は創出できない。 " POSITION="right"/>
+</node>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXのアクティビティのベース（土台）" POSITION="right">
+<node TEXT="AIの概念" POSITION="right">
+<node TEXT="AI白書2019" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+</node>
+<node TEXT="データサイエンス・AI" POSITION="right">
+<node TEXT="フェーズ" POSITION="right">
+<node TEXT="①データから学習によってモデルを作成する" POSITION="right"/>
+<node TEXT="②学習後のモデルを用いて新たなデータに対し推論を行う" POSITION="right"/>
+</node>
+<node TEXT="学習" POSITION="right"/>
+<node TEXT="利用" POSITION="right"/>
+<node TEXT="AI" POSITION="right">
+<node TEXT="機械学習" POSITION="right"/>
+<node TEXT="ディープラーニング" POSITION="right"/>
+</node>
+</node>
+<node TEXT="セキュリティ" POSITION="right">
+<node TEXT="サイバー・フィジカル・セキュリティ対策フレームワーク" POSITION="right">
+<node TEXT="" POSITION="right"/>
+</node>
+</node>
+<icon BUILTIN="full-2"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DXを実行する上での現状と課題" POSITION="left">
+<node TEXT="レガシーシステム" POSITION="left">
+<node TEXT="" POSITION="left"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="旧態依然の組織運営" POSITION="left">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="対応策" POSITION="left">
+<node TEXT="DX推進システムガイドライン" POSITION="left" LINK="http://www.meti.go.jp/press/2018/12/20181212004/20181212004-1.pdf">
+<node TEXT="DXを推進するための新たなデジタル技術の活用とレガシーシステム刷新に関するガイドライン" POSITION="left"/>
+<node TEXT="■経営戦略におけるDXの位置づけ" POSITION="left"/>
+<node TEXT="■DX実現に向けた新たなデジタル技術の活用やレガシーシステム刷新のための適切な体制・仕組み" POSITION="left"/>
+<node TEXT="■DX実現に向けた新たなデジタル技術の活用やレガシーシステム刷新のための適切な体制・仕組み" POSITION="left"/>
+<node TEXT="■DX実現に向けた新たなデジタル技術の活用やレガシーシステム刷新のための実行プロセス" POSITION="left"/>
+</node>
+<node TEXT="DXレポート～ITシステム「2025年の崖」の克服とDXの本格的な展開" POSITION="left">
+<node TEXT="2.4 ユーザ企業とベンダー企業との関係" POSITION="left">
+<node TEXT="ユーザ企業からベンダー企業への丸投げ" POSITION="left"/>
+<node TEXT="ユーザ企業とベンダー企業の責任関係" POSITION="left"/>
+<node TEXT="アジャイル開発における契約関係上のリスク" POSITION="left"/>
+</node>
+</node>
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DXの観点での一例" POSITION="left">
+<node TEXT="" POSITION="left"/>
+<icon BUILTIN="full-2"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DAX40-02 電子図書館サービスの始動" POSITION="left" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-02 電子図書館サービスの始動.html&quot;">
+<node TEXT="電子図書館の目指してきたところは、 昨今の「Society5.0」が目指してきた方向性と同じ。 更に、 デジタルアーカイブはデジタルトランスフォーメーションそのもの。 " POSITION="left">
+<node TEXT="Society5.0" POSITION="left">
+<node TEXT="Society5.0では、 サイバー空間とフィジカル空間を高度に融合させることにより、 多様なニーズにきめ細かく対応したモノやサービスを提供し、 経済的発展と社会的課題の解決を両立させる世界" POSITION="left"/>
+<node TEXT="Society5.0に向けて、 様々なつながりによる新たな価値社会を創出するConnected Industriesの実現に向けた当たらな産業構造の構築が必要" POSITION="left"/>
+</node>
+</node>
+<node TEXT="電子図書館（デジタルアーカイブ）の歩み" POSITION="left">
+<node TEXT="" POSITION="left"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DAX40-03 電子図書館サービスから知識インフラの構築へ" POSITION="right" COLOR="#17375e">
+<node TEXT="" POSITION="right"/>
+<node TEXT="DAX40-03-01 電子図書館サービスの実用化" POSITION="right" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-03-01 電子図書館サービスの実用化.html&quot; 電子図書館サービスの実用化.html"/>
+<node TEXT="DAX40-03-02 知識インフラの構築へ" POSITION="right" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-03-01 電子図書館サービスの実用化.html.html&quot; 知識インフラの構築へ.html"/>
+</node>
+<node TEXT="DAX40-05 「知の共有化」の実現を目指した構想【2013年】" POSITION="left" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-05 「知の共有化」の実現を目指した構想【2013年】.html&quot;">
+<node TEXT="デジタルアーカイブの概念" POSITION="left">
+<node TEXT="" POSITION="left"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DAX40-06 「知の共有化」システムの標準的な構築プロセスと必要なスキル・知識" POSITION="right">
+<node TEXT="DAX40-06-1 「知の共有化」システムの標準的な構築プロセス" POSITION="right" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-06-1 「知の共有化」システムの標準的な構築プロセス.html&quot;">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DAX40-06-2 政府情報システムでの標準的な構築プロセス" POSITION="right" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-06-2 政府情報システムでの標準的な構築プロセス.html&quot;">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="DAX40-06-3 「知の共有化」システムの構築に必要なスキル・知識" POSITION="right" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-06-3 「知の共有化」システムの構築に必要なスキル・知識.html&quot;">
+<icon BUILTIN="full-2"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="DAX40-10 【2019年】今後10年の図書館を取り巻く環境の社会環境の変化と図書館サービスシステムの方向性" POSITION="left" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-10 【2019年】今後10年の図書館を取り巻く環境の社会環境の変化と図書館サービスシステムの方向性.html&quot;">
+<node TEXT="Society5.0にマッピングした図書館サービス" POSITION="left">
+<node TEXT="" POSITION="left"/>
+</node>
+<node TEXT="次世代のコンテンツ創造・活用システムの構築" POSITION="left">
+<node TEXT="" POSITION="left"/>
+</node>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="未来の図書館" POSITION="left">
+<node TEXT="「未来の図書館を作るとは」（長尾真）より抜粋" POSITION="left"/>
+<node TEXT="次世代サービスイメージ" POSITION="left"/>
+<node TEXT="未来の図書館に必要な機能" POSITION="left"/>
+<node TEXT="構築・運用に必要な人材" POSITION="left"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="参考資料" POSITION="left">
+<node TEXT="DAX40-40 【補足資料】「見たことのない図書館を考える（2015年中山）」の実現性は？" POSITION="left" LINK="&quot;https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-40 「見たことのない図書館を考える（2015年中山）」の実現性は？.html&quot;"/>
+<node TEXT="DAX40-50 【参考】次世代サービス構築及び提供に当たって留意してほしいこと" POSITION="left" LINK="https://bluemoon55.github.io/Sharing_Knowledge/MindManager/DAX40-50%20%E3%80%90%E5%8F%82%E8%80%83%E3%80%91%E6%AC%A1%E4%B8%96%E4%BB%A3%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E6%A7%8B%E7%AF%89%E5%8F%8A%E3%81%B3%E6%8F%90%E4%BE%9B%E3%81%AB%E5%BD%93%E3%81%9F%E3%81%A3%E3%81%A6%E7%95%99%E6%84%8F%E3%81%97%E3%81%A6%E3%81%BB%E3%81%97%E3%81%84%E3%81%93%E3%81%A8.html"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</map>
